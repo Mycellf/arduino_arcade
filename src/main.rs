9429221e7e09eec4b15a7af17bd628606f9e6705
jeffrey <jcouch210@gmail.com>
2026-02-21 16:08:51 -0900

    Add frame timing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 #![no_std]
 #![no_main]
+#![feature(abi_avr_interrupt)]
 
 pub mod characters;
 pub mod lcd;
 pub mod overworld;
+pub mod time;
 
 use arduino_hal::hal::port::{PB2, PB3, PB4, PD2, PD3, PD4, PD5};
+use avr_device::interrupt;
 use panic_halt as _;
+use ufmt::uWrite;
 
 use crate::{
     characters::CHARACTERS,
@@ -19,11 +23,21 @@ use crate::{
 /// That's too many to type out all the time
 pub type LCD = lcd::LCD<PB4, PB2, PB3, PD5, PD4, PD3, PD2>;
 
+pub const FPS: u32 = 60;
+pub const FRAME_TIME_MILLISECONDS: u32 = 1000 / FPS;
+pub const TOTAL_FRAME_TIME_DEFICIT: u32 = 1000 - FRAME_TIME_MILLISECONDS * FPS;
+pub const DEFICIT_NUMERATOR: u32 = 2;
+pub const DEFICIT_DENOMINATOR: u32 = 3;
+const _: () = assert!(DEFICIT_NUMERATOR * FPS == TOTAL_FRAME_TIME_DEFICIT * DEFICIT_DENOMINATOR);
+
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    // let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+
+    time::millis_init(dp.TC0);
+    unsafe { interrupt::enable() };
 
     let mut lcd = LCD {
         register_select: pins.d12.into_output(),
@@ -44,9 +58,35 @@ fn main() -> ! {
         lcd.create_character(i as u8, character);
     }
 
-    overworld::print_screen(&mut lcd, 0);
+    lcd.set_cursor(0, 1);
+    lcd.print(TOTAL_FRAME_TIME_DEFICIT);
+
+    let mut deficit = 0u8;
+
+    let mut frames = 0u32;
 
     loop {
-        arduino_hal::delay_ms(1000);
+        let start = time::millis();
+
+        lcd.set_cursor(0, 0);
+        lcd.print(frames / 60);
+        frames += 1;
+
+        let frame_time = if deficit >= DEFICIT_NUMERATOR as u8 {
+            FRAME_TIME_MILLISECONDS
+        } else {
+            FRAME_TIME_MILLISECONDS + 1
+        };
+
+        deficit += 1;
+        deficit %= DEFICIT_DENOMINATOR as u8;
+
+        let elapsed = time::millis() - start;
+
+        if elapsed < frame_time {
+            arduino_hal::delay_ms(frame_time - elapsed);
+        } else {
+            serial.write_str("Frame too long").unwrap();
+        }
     }
 }
