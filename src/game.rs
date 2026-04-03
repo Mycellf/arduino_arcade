7f9d49776f40956c922e04cf9523962a2d93c07b
Elisha Helmericks <wboomfixtior.net@gmail.com>
2026-04-03 08:38:00 -0800

    new game moonLander
    added a new game

--- src/game.rs
+++ src/game.rs
@@ -1,9 +1,11 @@
+use core::arch::naked_asm;
+
 use embedded_hal::digital::InputPin;
 
 use crate::{
     game::{
         black_jack::BlackJack, block_catch::BlockCatch, overworld::Overworld, sokoban::Sokoban,
-        space_shooter::SpaceShooter,
+        space_shooter::SpaceShooter, note_beat::NoteBeat,
     },
     LCD,
 };
@@ -14,6 +16,7 @@ pub mod overworld;
 pub mod position;
 pub mod sokoban;
 pub mod space_shooter;
+mod note_beat;
 
 pub struct Game<Right: InputPin, Up: InputPin, Left: InputPin, Down: InputPin> {
     pub repeat_time: [i8; 2],
@@ -38,6 +41,7 @@ pub enum GameMode {
     BlackJack(BlackJack),
     SpaceShooter(SpaceShooter),
     Sokoban(Sokoban),
+    NoteBeat(NoteBeat),
 }
 
 impl GameMode {
@@ -48,6 +52,7 @@ impl GameMode {
             GameMode::BlackJack(_) => 1,
             GameMode::SpaceShooter(_) => 2,
             GameMode::Sokoban(_) => return None,
+            GameMode::NoteBeat(_) => return None,
         })
     }
 }
@@ -81,6 +86,7 @@ impl<Right: InputPin, Up: InputPin, Left: InputPin, Down: InputPin> Game<Right, 
             GameMode::BlackJack(black_jack) => black_jack.draw_full_screen(lcd),
             GameMode::SpaceShooter(space_shooter) => space_shooter.draw_full_screen(lcd),
             GameMode::Sokoban(sokoban) => sokoban.draw_full_screen(lcd),
+            GameMode::NoteBeat(note_beat) => note_beat.draw_full_screen(lcd),
         }
     }
 
@@ -97,6 +103,7 @@ impl<Right: InputPin, Up: InputPin, Left: InputPin, Down: InputPin> Game<Right, 
             GameMode::BlackJack(black_jack) => black_jack.update(lcd, raw_input, soft_input),
             GameMode::SpaceShooter(space_shooter) => space_shooter.update(lcd, raw_input),
             GameMode::Sokoban(sokoban) => sokoban.update(lcd, raw_input, soft_input),
+            GameMode::NoteBeat(note_beat) => note_beat.update(lcd, raw_input, soft_input),
         };
 
         if let Some(mode) = new_mode {
@@ -192,6 +199,7 @@ impl<Right: InputPin, Up: InputPin, Left: InputPin, Down: InputPin> Game<Right, 
             }
             GameMode::SpaceShooter(space_shooter) => space_shooter.score,
             GameMode::Sokoban(_) => 0,
+            GameMode::NoteBeat(_) => 0,
         }
     }
 }
--- src/game/note_beat.rs
+++ src/game/note_beat.rs
@@ -6,7 +6,7 @@
 //dificulty is number in the corner slowly going down, the time left to make an action 
 
 //use core::ops::{Index, IndexMut, Range};
-//use arduino_hal::prelude::_unwrap_infallible_UnwrapInfallible;
+use arduino_hal::prelude::_unwrap_infallible_UnwrapInfallible;
 use ufmt::uwrite;
 
 use crate::{
@@ -14,7 +14,15 @@ use crate::{
     rng, utils, LCD,
 };
 
+
 pub struct NoteBeat{
+    pub position_left: Position,
+    pub position_self: Position,
+    pub generated_array: [Directions; 8],
+    //pub size_of_array: u8,
+
+
+
     // pub spawn_timer: u8,
     // pub move_timer: u8,
     // pub score: u8,
@@ -22,10 +30,14 @@ pub struct NoteBeat{
 //array of positons or somthing for 
 }
 
-impl default for NoteBeat{
+impl Default for NoteBeat{
     fn default() -> Self{
+        //uwrite!(serial, "Hello world noteBeat");
         Self{
-            uwrite!(serial, "Hello world noteBeat"),
+            position_left: Position::new(0, 0),
+            position_self: Position::new(7, 1),
+            generated_array: [none; 8],
+            
             // spawn_timer: 0,
             // move_timer: 0,
             // score: 0,
@@ -36,17 +48,56 @@ impl default for NoteBeat{
     }
 
 }
-// pub enum directions{
-//     left, right, none,
+pub enum Directions{
+    left, right, none,
 
     
-// }
-
-pub fn draw_full_screen() {
-   uwrite!(serial, "draw full screen called");
 }
 
-pub fn update(){
-    uwrite!(serial, "update called");
-}
+impl NoteBeat {
+    pub fn draw_full_screen(&mut self, lcd: &mut LCD) {
+    
+        lcd.set_cursor(self.position_left);
+        lcd.clear();
+        uwrite!(lcd.fmt(), "Hello, world!").unwrap_infallible();
+    }
 
+    fn parse_queue(direction_list: [Directions; 8])-> &str {
+        let mut name="";
+        
+        for i in 0..size_of(){
+            if(direction_list[i]==Directions::right){
+                name = name + "R";
+            }else{
+                name = name + " ";
+            }
+            if(direction_list[i]==Directions::left){
+                name = "L"+ name;
+            }else{
+                name = " "+ name;
+            }
+        }
+        return name;
+
+    }
+
+    pub fn update(&mut self, lcd: &mut LCD, raw_input: [i8; 2], soft_input: [i8; 2]) -> Option<GameMode> {
+        
+        lcd.set_cursor(self.position_left);
+        lcd.clear();
+        let incoming = parse_queue(generated_array);
+        uwrite!(lcd.fmt(), "{}", incoming).unwrap_infallible();
+        lcd.set_cursor(self.position_self);
+        uwrite!(lcd.fmt(), "pl").unwrap_infallible();
+
+        if raw_input[0]> 0 {
+        uwrite!(lcd.fmt(), "0+").unwrap_infallible();
+
+        }else if raw_input[1]> 0  {
+        uwrite!(lcd.fmt(), "1+").unwrap_infallible();
+        }
+
+
+        None
+    }
+}
